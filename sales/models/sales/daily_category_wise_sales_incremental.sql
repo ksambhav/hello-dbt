b2abde05ab4ead9f2ba02afb4b2dee02e5c2c53f
sambhav.jain <sambhav.jain@hub.com>
2025-01-02 21:17:40 +0530

    Bug fix

--- sales/models/sales/daily_category_wise_sales_incremental.sql
+++ sales/models/sales/daily_category_wise_sales_incremental.sql
@@ -1,11 +1,6 @@
 --https://docs.getdbt.com/docs/build/incremental-models
 {{ config(materialized='incremental') }}
-
-WITH latest_existing_data AS (
-    SELECT coalesce(MAX(business_date),'1970-01-01') AS max_order_date
-    FROM {{ this }}
-),
-sales AS (
+WITH sales AS (
     SELECT
         date(order_date_time) as sales_date,
         amount,
@@ -19,5 +14,5 @@ sum(amount) over(partition by category) as total_sales
 from sales
 {% if is_incremental() %}
     -- this filter will only be applied on an incremental run
-    where sales_date > (select max_order_date from latest_existing_data )
+    where sales_date > (SELECT coalesce(MAX(business_date),'1970-01-01') AS max_order_date FROM {{ this }} )
 {% endif %}
